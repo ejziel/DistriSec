a68454b524473c11f212403d2f45bb087e3c3812
ejziel <ziel_ss@hotmail.com>
2020-12-14 09:45:39 -0300

    v1.1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://34.229.72.41:3333',
+    baseURL: 'http://distrisec-1580625634.us-east-1.elb.amazonaws.com:3333',
 })
 
 export default api;
